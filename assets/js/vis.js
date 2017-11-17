20c640ec90af14eccfc5ee3e671a5e117b2c490e
apatronl <ale7feb@hotmail.com>
2017-11-17 18:33:05 -0500

    Organized candy data and added all joy logos

--- assets/js/vis.js
+++ assets/js/vis.js
@@ -29,54 +29,198 @@ var candyMapSVG = d3.select("div#candyMapContainer")
 var keys = {country: 'Q4_COUNTRY', state: 'Q5_STATE_PROVINCE_COUNTY_ETC'};
 var feelings = {joy: 'JOY', meh: 'MEH', despair: 'DESPAIR'};
 
-var candyDataToCandyName = {
-    Q6_Butterfinger: "Butterfinger",
-    Q6_Candy_Corn: "Candy Corn",
-    Q6_Chiclets: "Chiclets",
-    Q6_Dots: "Dots",
-    Q6_Fuzzy_Peaches: "Fuzzy Peaches",
-    Q6_Good_N_Plenty: "Good & Plenty",
-    Q6_Gummy_Bears_straight_up: "Gummy Bears",
-    Q6_Healthy_Fruit: "Healthy Fruit",
-    Q6_Heath_Bar: "Heath Bar",
-    Q6_Hershey_s_Dark_Chocolate: "Hershey's Dark Chocolate",
-    Q6_Hershey_s_Kisses: "Hershey's Kisses",
-    Q6_Hershey_s_Milk_Chocolate: "Hershey's Milk Chocolate",
-    Q6_Jolly_Rancher_bad_flavor: "Jolly Rancher (Bad Flavor)",
-    Q6_Jolly_Ranchers_good_flavor: "Jolly Rancher (Good Flavor)",
-    Q6_Junior_Mints: "Junior Mints",
-    Q6_Kit_Kat: "Kit Kat",
-    Q6_LaffyTaffy: "Laffy Taffy",
-    Q6_LemonHeads: "LemonHeads",
-    Q6_Licorice_not_black: "Licorice (not black)",
-    Q6_Licorice_yes_black: "Licorice (black)",
-    Q6_Lollipops: "Lollipops",
-    Q6_Mike_and_Ike: "Mike and Ike",
-    Q6_Milk_Duds: "Milk Duds",
-    Q6_Milky_Way: "Milky Way",
-    Q6_Mint_Kisses: "Mint Kisses",
-    Q6_Mr_Goodbar: "Mr. Goodbar",
-    Q6_Nerds: "Nerds",
-    Q6_Nestle_Crunsh: "Crunch",
-    Q6_Peanut_M_M_s: "Peanut M&M's",
-    Q6_Peeps: "Peeps",
-    Q6_Pixy_Stix: "Pixy Stix",
-    Q6_Reese_s_Peanut_Butter_Cups: "Reese's",
-    Q6_Reese_s_Pieces: "Reese's Pieces",
-    Q6_Regular_M_Ms: "Milk Chocolate M&M's",
-    Q6_Rolos: "Rolos",
-    Q6_Skittles: "Skittles",
-    Q6_Snickers: "Snickers",
-    Q6_Sourpatch_Kids_i_e_abominations_of_nature: "Sourpatch Kids",
-    Q6_Starburst: "Starburst",
-    Q6_Swedish_Fish: "Swedish Fish",
-    Q6_Three_Musketeers: "3 Musketeers",
-    Q6_Tic_Tacs: "Tic Tacs",
-    Q6_Tolberone_something_or_other: "Toblerone",
-    Q6_Trail_Mix: "Trail Mix",
-    Q6_Twix: "Twix",
-    Q6_Whatchamacallit_Bars: "Whatchamacallit Bars",
-    Q6_York_Peppermint_Patties: "York"
+var candyData = {
+    Q6_Butterfinger: {
+        key: "Q6_Butterfinger",
+        name: "Butterfinger",
+        img: "butterfinger.png",
+        id: "butterfinger"
+    },
+    Q6_Candy_Corn: {
+        key: "Q6_Candy_Corn",
+        name:"Candy Corn"
+    },
+    Q6_Chiclets: {
+        key: "Q6_Chiclets",
+        name: "Chiclets"
+    },
+    Q6_Dots: {
+        key: "Q6_Dots",
+        name: "Dots",
+        img: "dots.jpg",
+        id: "dots"
+    },
+    Q6_Fuzzy_Peaches: {
+        key: "Q6_Fuzzy_Peaches",
+        name: "Fuzzy Peaches"
+    },
+    Q6_Good_N_Plenty: {
+        key: "Q6_Good_N_Plenty",
+        name: "Good & Plenty"
+    },
+    Q6_Gummy_Bears_straight_up: {
+        key: "Q6_Gummy_Bears_straight_up",
+        name: "Gummy Bears"
+    },
+    Q6_Heath_Bar: {
+        key: "Q6_Heath_Bar",
+        name: "Heath Bar"
+    },
+    Q6_Hershey_s_Dark_Chocolate: {
+        key: "Q6_Hershey_s_Dark_Chocolate",
+        name: "Hershey's Dark Chocolate"
+    },
+    Q6_Hershey_s_Kisses: {
+        key: "Q6_Hershey_s_Kisses",
+        name: "Hershey's Kisses"
+    },
+    Q6_Hershey_s_Milk_Chocolate: {
+        key: "Q6_Hershey_s_Milk_Chocolate",
+        name: "Hershey's Milk Chocolate"
+    },
+    Q6_Jolly_Rancher_bad_flavor: {
+        key: "Q6_Jolly_Rancher_bad_flavor",
+        name: "Jolly Rancher (Bad Flavor)"
+    },
+    Q6_Jolly_Ranchers_good_flavor: {
+        key: "Q6_Jolly_Ranchers_good_flavor",
+        name: "Jolly Rancher (Good Flavor)"
+    },
+    Q6_Junior_Mints: {
+        key: "Q6_Junior_Mints",
+        name: "Junior Mints"
+    },
+    Q6_Kit_Kat: {
+        key: "Q6_Kit_Kat",
+        name: "Kit Kat",
+        img: "kitkat.jpg",
+        id: "kitkat"
+    },
+    Q6_LaffyTaffy: {
+        key: "Q6_LaffyTaffy",
+        name: "Laffy Taffy"
+    },
+    Q6_LemonHeads: {
+        key: "Q6_LemonHeads",
+        name: "LemonHeads"
+    },
+    Q6_Licorice_not_black: {
+        key: "Q6_Licorice_not_black",
+        name: "Licorice (not black)"
+    },
+    Q6_Licorice_yes_black: {
+        key: "Q6_Licorice_yes_black",
+        name: "Licorice (black)"
+    },
+    Q6_Lollipops: {
+        key: "Q6_Lollipops",
+        name: "Lollipops"
+    },
+    Q6_Mike_and_Ike: {
+        key: "Q6_Mike_and_Ike",
+        name: "Mike and Ike"
+    },
+    Q6_Milk_Duds: {
+        key: "Q6_Milk_Duds",
+        name: "Milk Duds"
+    },
+    Q6_Milky_Way: {
+        key: "Q6_Milky_Way",
+        name: "Milky Way",
+        img: "milkyway.jpg",
+        id: "milkyway"
+    },
+    Q6_Mint_Kisses: {
+        key: "Q6_Mint_Kisses",
+        name: "Mint Kisses"
+    },
+    Q6_Mr_Goodbar: {
+        key: "Q6_Mr_Goodbar",
+        name: "Mr. Goodbar"
+    },
+    Q6_Nerds: {
+        key: "Q6_Nerds",
+        name: "Nerds"
+    },
+    Q6_Nestle_Crunsh: {
+        key: "Q6_Nestle_Crunsh",
+        name: "Crunch"
+    },
+    Q6_Peanut_M_M_s: {
+        key: "Q6_Peanut_M_M_s",
+        name: "Peanut M&M's"
+    },
+    Q6_Peeps: {
+        key: "Q6_Peeps",
+        name: "Peeps"
+    },
+    Q6_Pixy_Stix: {
+        name: "Pixy Stix"
+    },
+    Q6_Reese_s_Peanut_Butter_Cups: {
+        name: "Reese's",
+        img: "reeses.jpg",
+        id: "reeses"
+    },
+    Q6_Reese_s_Pieces: {
+        name: "Reese's Pieces",
+        img: "reeses.jpg",
+        id: "reesespieces"
+    },
+    Q6_Regular_M_Ms: {
+        name: "Milk Chocolate M&M's"
+    },
+    Q6_Rolos: {
+        name: "Rolos"
+    },
+    Q6_Skittles: {
+        name: "Skittles"
+    },
+    Q6_Snickers: {
+        name: "Snickers",
+        img: "snickers.jpg",
+        id: "snickers"
+    },
+    Q6_Sourpatch_Kids_i_e_abominations_of_nature: {
+        name: "Sourpatch Kids"
+    },
+    Q6_Starburst: {
+        name: "Starburst"
+    },
+    Q6_Swedish_Fish: {
+        name: "Swedish Fish"
+    },
+    Q6_Three_Musketeers: {
+        name: "3 Musketeers"
+    },
+    Q6_Tic_Tacs: {
+        name: "Tic Tacs"
+    },
+    Q6_Tolberone_something_or_other: {
+        name: "Toblerone",
+        img: "toblerone.png",
+        id: "toblerone"
+    },
+    Q6_Trail_Mix: {
+        key: "Q6_Trail_Mix",
+        name: "Trail Mix"
+    },
+    Q6_Twix: {
+        key: "Q6_Twix",
+        name: "Twix",
+        img: "twix.jpg",
+        id: "twix"
+    },
+    Q6_Whatchamacallit_Bars: {
+        key: "Q6_Whatchamacallit_Bars",
+        name: "Whatchamacallit Bars"
+    },
+    Q6_York_Peppermint_Patties: {
+        key: "Q6_York_Peppermint_Patties",
+        name: "York Peppermint Patties",
+        img: "york-peppermint-patties.jpg",
+        id: "york"
+    }
 };
 
 var candyToColor = {
@@ -109,44 +253,24 @@ var candyToColor = {
     Q6_Nerds: "#8975b0"
 };
 
-// var drag = d3.behavior.drag()
-//     .origin(function(d) { return d })
-//     .on('drag', function(d, i) {
-//         d.x += d3.event.dx
-//         d.y += d3.event.dy
-//         d3.select(this).attr("transform", function(d,i) {
-//             return "translate(" + [d.x, d.y] + ")"
-//         })
-//     })
-//     .on('dragend', function () {
-//         console.log('drag ended');
-//     });
-
-// function dragmove(d) {
-//     if (!d.pinch) {
-//
-//     }
-// }
-
 d3.csv('./data/candy.csv', function(error, dataset) {
     if (error) {
         console.error('Error while loading ./data/candy.csv dataset.');
         console.error(error);
         return;
     }
-    console.log(dataset);
+
     dataByCandy = [];
-    Object.keys(candyDataToCandyName).forEach(function(candy, i) {
-        candyData = {};
-        candyData[candy] = {joy: 0, meh: 0, despair: 0};
+    Object.keys(candyData).forEach(function(candy, i) {
+        candyDataDict = {};
+        candyDataDict[candy] = {joy: 0, meh: 0, despair: 0};
         dataset.forEach(function(d, j) {
             feeling = d[candy];
-            if (feeling) candyData[candy][feeling.toLowerCase()] += 1;
+            if (feeling) candyDataDict[candy][feeling.toLowerCase()] += 1;
         });
-        dataByCandy[i] = candyData;
+        dataByCandy[i] = candyDataDict;
     });
     console.log(dataByCandy);
-
     dataByState = d3.nest()
         .key(function(d) {
             var country = d[keys.country];
@@ -162,15 +286,15 @@ d3.csv('./data/candy.csv', function(error, dataset) {
             };
             Object.keys(candyFeeling).forEach(function(feeling, i) {
                 candyDataArr = [];
-                Object.keys(candyDataToCandyName).forEach(function(candy, i) {
-                    candyData = {};
-                    candyData.candy = candy;
-                    candyData.value = 0;
-                    candyFeeling[feeling].push(candyData)
+                Object.keys(candyData).forEach(function(candy, i) {
+                    candyDataDict = {};
+                    candyDataDict.candy = candy;
+                    candyDataDict.value = 0;
+                    candyFeeling[feeling].push(candyDataDict)
                 });
             });
             state.forEach(function(datapoint, i) {
-                Object.keys(candyDataToCandyName).forEach(function(candy, i) {
+                Object.keys(candyData).forEach(function(candy, i) {
                     var currFeeling = datapoint[candy];
                     if (currFeeling == 'JOY' || currFeeling == 'MEH' || currFeeling == 'DESPAIR') {
                         candyFeeling[currFeeling][i].value += 1;
@@ -260,54 +384,22 @@ function drawMap(data) {
            }
         }
 
-        var defs = candyMapSVG.append('defs')
-        defs.append('pattern')
-            .attr('id', 'kitkat')
-            .attr('patternUnits', 'userSpaceOnUse')
-            .attr('width', 95.5)
-            .attr('height', 100)
-            .append('svg:image')
-            .attr('xlink:href', 'img/kitkat.jpg')
-            .attr('width', 100)
-            .attr('height', 100)
-            .attr('x', 0)
-            .attr('y', 0);
-
-        defs.append('pattern')
-            .attr('id', 'milkyway')
-            .attr('patternUnits', 'userSpaceOnUse')
-            .attr('width', 95.5)
-            .attr('height', 100)
-            .append('svg:image')
-            .attr('xlink:href', 'img/milkyway.jpg')
-            .attr('width', 180)
-            .attr('height', 180)
-            .attr('x', 0)
-            .attr('y', 0);
-
-        defs.append('pattern')
-            .attr('id', 'butterfinger')
-            .attr('patternUnits', 'userSpaceOnUse')
-            .attr('width', 95.5)
-            .attr('height', 100)
-            .append('svg:image')
-            .attr('xlink:href', 'img/butterfinger.png')
-            .attr('width', 100)
-            .attr('height', 100)
-            .attr('x', 0)
-            .attr('y', 0);
-
-        defs.append('pattern')
-            .attr('id', 'reeses')
-            .attr('patternUnits', 'userSpaceOnUse')
-            .attr('width', 95.5)
-            .attr('height', 100)
-            .append('svg:image')
-            .attr('xlink:href', 'img/reeses.jpg')
-            .attr('width', 100)
-            .attr('height', 100)
-            .attr('x', 0)
-            .attr('y', 0);
+        var defs = candyMapSVG.append('defs');
+        Object.keys(candyData).forEach(function(candy) {
+            if (candyData[candy].img) {
+                defs.append('pattern')
+                    .attr('id', function(d) { return candyData[candy].id; })
+                    .attr('patternUnits', 'userSpaceOnUse')
+                    .attr('width', 95.5)
+                    .attr('height', 100)
+                    .append('svg:image')
+                    .attr('xlink:href', function(d) { return 'img/' + candyData[candy].img; })
+                    .attr('width', 100)
+                    .attr('height', 100)
+                    .attr('x', 0)
+                    .attr('y', 0);
+            }
+        });
 
         var state = candyMapSVG.selectAll('g.state')
             .data(json.features);
@@ -324,15 +416,11 @@ function drawMap(data) {
                 var stateName = d.properties.name;
                 var topJoy = d.properties.top_joy;
                 if (!topJoy) return '#888';
-                if (topJoy.candy == 'Q6_Kit_Kat') return 'url(#kitkat)';
-                if (topJoy.candy == 'Q6_Milky_Way') return 'url(#milkyway)';
-                if (topJoy.candy == 'Q6_Butterfinger') return 'url(#butterfinger)';
-                if (topJoy.candy == 'Q6_Reese_s_Peanut_Butter_Cups') return 'url(#reeses)';
-                var color = candyToColor[topJoy.candy];
-                if (!color) return '#888';
-                if (!color.startsWith('#')) return '#888';
-                return color != undefined ? color : '#888';
-                // return "url(#pic1)";
+                var candy = topJoy.candy;
+                if (candyData[candy].img) {
+                    return 'url(#' + candyData[candy].id + ')';
+                }
+                return '#888';
             })
             .on("mouseover", function(d) {
                 d3.select(this).style('opacity', '0.5');
